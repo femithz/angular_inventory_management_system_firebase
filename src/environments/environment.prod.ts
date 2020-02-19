2142a79cee74bf17325d2dd413a5e373bb743651
Ajayi Oluwafemi Adeyinka <ajayiadeyinka6991@gmail.com>
2020-02-19 12:37:11 +0100

    build output direction corrected

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCiNT8dYO-9qwwgKa3nwGkGPCs21gItUKc",
+    authDomain: "inventory-management-sys-6f72d.firebaseapp.com",
+    databaseURL: "https://inventory-management-sys-6f72d.firebaseio.com",
+    projectId: "inventory-management-sys-6f72d",
+    storageBucket: "inventory-management-sys-6f72d.appspot.com",
+    messagingSenderId: "864539567583",
+    appId: "1:864539567583:web:0a7021e22935ad1bba106a"
+}
 };
